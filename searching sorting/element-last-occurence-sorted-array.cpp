c8e6478960bf15fe66194b8ec1a9b1e144f0992d
Rishav Chandra <chandrarishav78@gmail.com>
2023-04-19 23:38:09 +0530

    target as input

--- searching sorting/element-last-occurence-sorted-array.cpp
+++ searching sorting/element-last-occurence-sorted-array.cpp
@@ -3,11 +3,12 @@ using namespace std;
              
 int main(){
     vector <int> v{1, 2, 5, 5, 5, 6, 11};
-    int target = 5;
+    int target;
     int start = 0;
     int end = v.size() - 1;
     int mid = start + (end - start) / 2;
-    int ans = -1;       
+    int ans = -1;  
+    cin >> target;     
     while(start <= end){
         if(v[mid] == target){
             ans = mid;
