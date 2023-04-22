1eca14e81f1d2638ed51e43924fc238f082fee15
Rishav Chandra <chandrarishav78@gmail.com>
2023-04-22 14:48:58 +0530

    added division-binary-search

---
+++ searching sorting/division-binary-search.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+             
+int main(){
+    int dividend = 20;
+    int divisor = 6;
+    int s = 0;
+    int e = abs(dividend);
+    int mid = s + (e-s)/2;
+    int ans = 0;
+    while(s <= e){
+        if(abs(mid*divisor) == abs(dividend)){
+            ans = mid;
+        }
+        else if(abs(mid*divisor) < abs(dividend)){
+            e = mid-1;
+        }
+        else{
+            ans=mid;
+            s=mid+1;
+        }
+         mid = s + (e-s)/2;
+    }         
+    if((divisor<0 && dividend<0) || (divisor>0 && dividend>0)){
+        cout << ans;
+    }
+    else{
+        cout << -ans;
+    }
+             
+    return 0;
+}
