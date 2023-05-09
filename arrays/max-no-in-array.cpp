fe9772c92698e859cabb7fe1dfa9d2ac59d704a2
Rishav Chandra <chandrarishav78@gmail.com>
2023-05-09 19:47:32 +0530

    input in max-no-in-array

--- arrays/max-no-in-array.cpp
+++ arrays/max-no-in-array.cpp
@@ -2,10 +2,15 @@
 using namespace std;
 
 int main(){
-    int arr[] = {1, 2, 6, 7, 9, 3, 6};
+    int size;
+    int arr[size];
+    cin >> size;
+    for(int i=0; i<size; i++){
+        cin >> arr[i];
+    }
 
     int max_num = INT_MIN;
-    for(int i=0; i<7; i++){
+    for(int i=0; i<size; i++){
         if(arr[i] > max_num) max_num = arr[i];
     }
     cout<<"Maximum number in the array is "<<max_num;
