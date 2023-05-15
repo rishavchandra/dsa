4be1ad338b50183d43748382489e176de9fe817d
Rishav Chandra <chandrarishav78@gmail.com>
2023-05-15 23:58:22 +0530

    updated matrix-transpose

--- arrays/matrix-transpose.cpp
+++ arrays/matrix-transpose.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 int main(){
-    int arr[][3] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+    int arr[][3] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 11}};
     int ans[3][3];
     for(int i = 0; i < 3; i ++){
         for(int j = 0; j < 3; j ++){
