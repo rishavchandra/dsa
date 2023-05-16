83a783216f8aba78d056fde46511306ad6de1a64
Rishav Chandra <chandrarishav78@gmail.com>
2023-05-17 00:21:19 +0530

    updated for user input

--- arrays/missing-element-in-array-of-duplicates.cpp
+++ arrays/missing-element-in-array-of-duplicates.cpp
@@ -2,8 +2,11 @@
 using namespace std;
 
 int main(){
-    int arr[] = {1, 2, 3, 3, 5};
-    int size = 5;
+    int size;
+    int arr[size];
+    for(int i=0; i<size; i++){
+        cin >> arr[i];
+    }
     for(int i = 0; i < size; i ++){
         int index = arr[i];
         if(arr[index - 1] > 0){
