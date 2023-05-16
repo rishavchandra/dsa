8926659219f6db86922f62677fb8c426b6a69579
Rishav Chandra <chandrarishav78@gmail.com>
2023-05-17 00:23:02 +0530

    updated code

--- arrays/majority-element-in-array.cpp
+++ arrays/majority-element-in-array.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main(){
     int arr[] = {1, 7, 9, 9, 9, 4, 7, 4, 9, 9, 9, 9};
-    int size = 12;
+    int size = sizeof(arr)/sizeof(int);
     int maxCount = 0;
     int index = INT_MIN;
     for(int i = 0; i < size; i ++){
