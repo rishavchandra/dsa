b900569f355470beaabcc0ea2a99dda6cb72dccf
Rishav Chandra <chandrarishav78@gmail.com>
2023-05-21 01:45:14 +0530

    updated file

--- replace-spaces.cpp
+++ replace-spaces.cpp
@@ -5,7 +5,6 @@ int main(){
     char str[50];
     cin.getline(str, 50);
     int size = strlen(str);
-    int i=0, j=size-1;
     for(int i=0; i<size; i++){
         if(str[i] == ' '){
             str[i] = '@';
