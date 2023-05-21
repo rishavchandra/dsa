b7daf7e8e151d070de64477dc54d98dcf8d77998
Rishav Chandra <chandrarishav78@gmail.com>
2023-05-21 12:20:08 +0530

    added remove-occurences-of-substring

---
+++ strings and char arrays/remove-all-occurences-of-substring.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+             
+int main(){
+      string s;
+      string sub;
+      cin >> s >> sub;
+      int pos = s.find(sub);
+    while(pos != string::npos){
+        s.erase(pos, sub.length());
+        pos = s.find(sub);
+    }
+    cout << s;       
+             
+    return 0;
+}
