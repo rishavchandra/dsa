743ea70d0c6162f3a5f1d6c890839002d869bb0a
Rishav Chandra <chandrarishav78@gmail.com>
2023-05-21 13:48:56 +0530

    added min-minutes-difference

---
+++ strings and char arrays/min-minutes-difference.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+             
+int main(){
+    vector<string> timePoints{"23:59","00:00"};
+    vector<int> minutes;
+        int minDiff = INT_MAX;
+        for(int i=0; i<timePoints.size(); i++){
+            string curr = timePoints[i];
+            int hrs = stoi(curr.substr(0, 2));
+            int mins = stoi(curr.substr(3, 2));
+            minutes.push_back(hrs*60 + mins);
+        }
+        sort(minutes.begin(), minutes.end());
+        for(int i=0; i<minutes.size()-1; i++){
+            if((minutes[i+1] - minutes[i]) < minDiff){
+                minDiff = minutes[i+1] - minutes[i];
+            }
+        }
+        int lastDiff = minutes[0]+1440 - minutes[minutes.size()-1];
+        if(lastDiff < minDiff) minDiff = lastDiff;  
+        cout << minDiff;      
+             
+    return 0;
+}
