f7b715eecb50990c13ee866e8ee9633134d94d82
Rishav Chandra <chandrarishav78@gmail.com>
2023-05-21 12:21:06 +0530

    cleanup

--- strings and char arrays/remove-all-occurences-of-substring.cpp
+++ strings and char arrays/remove-all-occurences-of-substring.cpp
@@ -2,10 +2,10 @@
 using namespace std;
              
 int main(){
-      string s;
-      string sub;
-      cin >> s >> sub;
-      int pos = s.find(sub);
+    string s;
+    string sub;
+    cin >> s >> sub;
+    int pos = s.find(sub);
     while(pos != string::npos){
         s.erase(pos, sub.length());
         pos = s.find(sub);
