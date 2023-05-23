8b77f0139d58d2c4df6c6729ecbc563c34f5e16e
Rishav Chandra <chandrarishav78@gmail.com>
2023-05-23 11:29:48 +0530

    cleanup

--- strings and char arrays/valid-palindrome-2.cpp
+++ strings and char arrays/valid-palindrome-2.cpp
@@ -1,26 +1,26 @@
 #include<bits/stdc++.h>
 using namespace std;
 
- bool checkPalindrome(string s, int i, int j){
-        while(i < j){
-            if(s[i] != s[j]) return false;
+bool checkPalindrome(string s, int i, int j){
+    while(i < j){
+        if(s[i] != s[j]) return false;
+        i++; j--;
+    }
+    return true;
+}
+bool validPalindrome(string s) {
+    int i = 0;
+    int j = s.length()-1;
+    while(i < j){
+        if(s[i] != s[j]){
+            return checkPalindrome(s, i+1, j) || checkPalindrome(s, i, j-1);
+        }
+        else{
             i++; j--;
         }
-        return true;
     }
-    bool validPalindrome(string s) {
-        int i = 0;
-        int j = s.length()-1;
-        while(i < j){
-            if(s[i] != s[j]){
-                return checkPalindrome(s, i+1, j) || checkPalindrome(s, i, j-1);
-            }
-            else{
-                i++; j--;
-            }
-        }
-        return true;
-    }            
+    return true;
+}            
 int main(){
     string a = "abca";
     cout << validPalindrome(a);    
