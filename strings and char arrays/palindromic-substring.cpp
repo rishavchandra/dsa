615fb6f61561b608b54b3538c1970f923307a11f
Rishav Chandra <chandrarishav78@gmail.com>
2023-05-25 17:34:16 +0530

    cleanup

--- strings and char arrays/palindromic-substring.cpp
+++ strings and char arrays/palindromic-substring.cpp
@@ -2,24 +2,26 @@
 using namespace std;
 
 int expandAroundIndex(string s, int i, int j){
-        int count = 0;
-        while(i>=0 && j<s.length() && s[i]==s[j]){
-            count++;
-            i--;
-            j++;
-        }
-        return count;
+    int count = 0;
+    while(i>=0 && j<s.length() && s[i]==s[j]){
+        count++;
+        i--;
+        j++;
     }
-    int countSubstrings(string s) {
-        int count = 0;
-        for(int i=0; i<s.length(); i++){
-            int odds = expandAroundIndex(s, i, i);
-            count += odds;
-            int evens = expandAroundIndex(s, i, i+1);
-            count += evens;
-        }
-        return count;
-    }             
+    return count;
+}
+
+int countSubstrings(string s) {
+    int count = 0;
+    for(int i=0; i<s.length(); i++){
+        int odds = expandAroundIndex(s, i, i);
+        count += odds;
+        int evens = expandAroundIndex(s, i, i+1);
+        count += evens;
+    }
+    return count;
+}
+             
 int main(){
     string s = "aaa";
     cout << countSubstrings(s);
