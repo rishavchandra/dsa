4955609afcedd333929442b2916365aab2a8d91d
Rishav Chandra <chandrarishav78@gmail.com>
2023-05-25 17:35:14 +0530

    cleaup

--- strings and char arrays/reverse-letters.cpp
+++ strings and char arrays/reverse-letters.cpp
@@ -2,17 +2,17 @@
 using namespace std;
 
 string reverseOnlyLetters(string s) {
-        int i=0, j=s.length()-1;
-        while(i<j){
-            if(isalpha(s[i]) && isalpha(s[j])){
-                swap(s[i], s[j]);
-                i++; j--;
-            }
-            else if(!isalpha(s[i])) i++;
-            else j--;
+    int i=0, j=s.length()-1;
+    while(i<j){
+        if(isalpha(s[i]) && isalpha(s[j])){
+            swap(s[i], s[j]);
+            i++; j--;
         }
-        return s;
+        else if(!isalpha(s[i])) i++;
+        else j--;
     }
+    return s;
+}
 
 int main(){
     string s = "Where is the greenish-yellow mug?";
