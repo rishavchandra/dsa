314c41fb82d348dfba548376129765fed2932da6
Rishav Chandra <chandrarishav78@gmail.com>
2023-05-25 17:32:36 +0530

    cleanup

--- strings and char arrays/longest-common-prefix.cpp
+++ strings and char arrays/longest-common-prefix.cpp
@@ -2,29 +2,29 @@
 using namespace std;
 
 string longestCommonPrefix(vector<string>& strs) {
-        string ans;
-        int i = 0;
-        while(true){
-            char curr_ch = 0;
-            for(auto str : strs){
-                if(i >= str.length()){
-                    curr_ch = 0;
-                    break;
-                }
-                if(curr_ch == 0){
-                    curr_ch = str[i];
-                }
-                else if(str[i] != curr_ch){
-                    curr_ch = 0;
-                    break;
-                }
+    string ans;
+    int i = 0;
+    while(true){
+        char curr_ch = 0;
+        for(auto str : strs){
+            if(i >= str.length()){
+                curr_ch = 0;
+                break;
             }
-            if(curr_ch == 0) break;
-            ans.push_back(curr_ch);
-            i++;
+            if(curr_ch == 0){
+                curr_ch = str[i];
+            }
+            else if(str[i] != curr_ch){
+                curr_ch = 0;
+                break;
+            }
         }
-        return ans;
-    }             
+        if(curr_ch == 0) break;
+        ans.push_back(curr_ch);
+        i++;
+    }
+    return ans;
+}             
 int main(){
     vector<string> v{"flower", "flow", "flight"};
     cout << longestCommonPrefix(v);     
