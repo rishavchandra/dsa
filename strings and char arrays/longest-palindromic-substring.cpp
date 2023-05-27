cc4f3597b2459632c3c38dd4f46451feaa1205bc
Rishav Chandra <chandrarishav78@gmail.com>
2023-05-27 21:08:18 +0530

    cleanup

--- strings and char arrays/longest-palindromic-substring.cpp
+++ strings and char arrays/longest-palindromic-substring.cpp
@@ -2,24 +2,24 @@
 using namespace std;
 
 bool isPalindrome(string s, int i, int j){
-        while(i<j){
-            if(s[i] != s[j]) return false;
-            i++; j--;
-        }
-        return true;
+    while(i<j){
+        if(s[i] != s[j]) return false;
+        i++; j--;
     }
-    string longestPalindrome(string s) {
-        string ans="";
-        for(int i=0; i<s.size(); i++){
-            for(int j=i; j<s.size(); j++){
-                if(isPalindrome(s, i, j)){
-                    string ss = s.substr(i, j-i+1);
-                    ans = ss.size() > ans.size() ? ss : ans;
-                }
+    return true;
+}
+string longestPalindrome(string s) {
+    string ans="";
+    for(int i=0; i<s.size(); i++){
+        for(int j=i; j<s.size(); j++){
+            if(isPalindrome(s, i, j)){
+                string ss = s.substr(i, j-i+1);
+                ans = ss.size() > ans.size() ? ss : ans;
             }
         }
-        return ans;
-    }             
+    }
+    return ans;
+}             
 int main(){
     cout << longestPalindrome("babad");         
              
