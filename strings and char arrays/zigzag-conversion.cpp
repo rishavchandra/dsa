637b8876850233614486aba79dc526c7495cff0e
Rishav Chandra <chandrarishav78@gmail.com>
2023-05-28 01:41:07 +0530

    cleanup

--- strings and char arrays/zigzag-conversion.cpp
+++ strings and char arrays/zigzag-conversion.cpp
@@ -2,31 +2,31 @@
 using namespace std;
 
 string convert(string s, int numRows) {
-        vector<string> zigzag(numRows);
-        int i=0, row=0;
-        bool direction=1;
-        while(true){
-            if(direction){
-                while(row<numRows && i<s.size()){
-                    zigzag[row++].push_back(s[i++]);
-                }
-                row = numRows-2;
+    vector<string> zigzag(numRows);
+    int i=0, row=0;
+    bool direction=1;
+    while(true){
+        if(direction){
+            while(row<numRows && i<s.size()){
+                zigzag[row++].push_back(s[i++]);
             }
-            else{
-                while(row>=0 && i<s.size()){
-                    zigzag[row--].push_back(s[i++]);
-                }
-                row = 1;
+            row = numRows-2;
+        }
+        else{
+            while(row>=0 && i<s.size()){
+                zigzag[row--].push_back(s[i++]);
             }
-            if(i>s.size()) break;
-            direction = !direction;
+        row = 1;
         }
-        string ans="";
-        for(int i=0; i<zigzag.size(); i++){
-            ans += zigzag[i];
-        }
-        return ans;
-    }             
+        if(i>s.size()) break;
+        direction = !direction;
+    }
+    string ans="";
+    for(int i=0; i<zigzag.size(); i++){
+        ans += zigzag[i];
+    }
+    return ans;
+}             
 int main(){
     cout << convert("PAYPALISHIRING", 3);         
              
