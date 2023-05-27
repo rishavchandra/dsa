dbb9064f59ba2c2e2c873e307a3201d2711e356d
Rishav Chandra <chandrarishav78@gmail.com>
2023-05-27 21:56:39 +0530

    cleanup

--- strings and char arrays/first-occurrence-index-in-string.cpp
+++ strings and char arrays/first-occurrence-index-in-string.cpp
@@ -2,21 +2,22 @@
 using namespace std;
 
 int strStr(string haystack, string needle) {
-        // int index = haystack.find(needle);
-        // if(index != string::npos) return index;
-        // else return -1;
+    // int index = haystack.find(needle);
+    // if(index != string::npos) return index;
+    // else return -1;
 
-        int n = haystack.size();
-        int m = needle.size();
+    int n = haystack.size();
+    int m = needle.size();
 
-        for(int i=0; i<=n-m; i++){
-            for(int j=0; j<m; j++){
-                if(needle[j] != haystack[i+j]) break;
-                if(j == m-1) return i;
-            }
+    for(int i=0; i<=n-m; i++){
+        for(int j=0; j<m; j++){
+            if(needle[j] != haystack[i+j]) break;
+            if(j == m-1) return i;
         }
-        return -1;
-    }             
+    }
+    return -1;
+
+}             
 int main(){
     cout << strStr("sadbutsad", "sad");
              
