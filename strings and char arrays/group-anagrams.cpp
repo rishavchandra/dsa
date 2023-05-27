a55a988f48fb5de3ed821c9e4750e344c1136bfa
Rishav Chandra <chandrarishav78@gmail.com>
2023-05-27 20:12:49 +0530

    cleanup

--- strings and char arrays/group-anagrams.cpp
+++ strings and char arrays/group-anagrams.cpp
@@ -2,18 +2,18 @@
 using namespace std;
 
 vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        map<string, vector<string>> mp;
-        for(auto str: strs){
-            string s = str;
-            sort(s.begin(), s.end());
-            mp[s].push_back(str);
-        }
-        vector<vector<string>> ans;
-        for(auto i=mp.begin(); i!=mp.end(); i++){
-            ans.push_back(i->second);
-        }
-        return ans;
-    }             
+    map<string, vector<string>> mp;
+    for(auto str: strs){
+        string s = str;
+        sort(s.begin(), s.end());
+        mp[s].push_back(str);
+    }
+    vector<vector<string>> ans;
+    for(auto i=mp.begin(); i!=mp.end(); i++){
+        ans.push_back(i->second);
+    }
+    return ans;
+}             
 int main(){
     vector<string> strs = {"eat","tea","tan","ate","nat","bat"};      
              
