fa997014bb68f6e7d73ed259446cd565e2a57b73
Rishav Chandra <112895379+rishavchandra@users.noreply.github.com>
2023-08-27 23:44:38 +0530

    cleanup

--- basic maths & pointers/prime-number.cpp
+++ basic maths & pointers/prime-number.cpp
@@ -4,15 +4,18 @@ using namespace std;
 int main(){
     int n;
     cin >> n;
-    bool isPrime = true;
-    for(int i=2; i<n; i++){
-        if(n%i == 0){
-            isPrime = false;
-            break;
-        }
-    }         
-    if(isPrime) cout << "Prime";
-    else cout << "Not Prime";
+    if(n == 1) cout << "Not Prime";
+    else{
+        bool isPrime = true;
+        for(int i=2; i<n; i++){
+            if(n%i == 0){
+                isPrime = false;
+                break;
+            }
+        }         
+        if(isPrime) cout << "Prime";
+        else cout << "Not Prime";
+    }
              
     return 0;
 }
